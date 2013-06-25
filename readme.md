1b40aea4fc215f4c9dc30789bb8aa58488295189
Thomas Levine <occurrence@thomaslevine.com>
2013-06-24 21:03:11 -0700

    test all the cases

--- readme.md
+++ readme.md
@@ -2,10 +2,17 @@ Microformats parser in Python
 ======
 This is a microformats parser in Python.
 
+# How to
+
     import microformats as ufs
     fp = open('index.html')
     data = ufs.parse(fp)
 
 `data` now contains the microformats data.
+
+# Running tests
+From the root of the repository,
+
+    nosetests
 
 Specs come from [here](https://github.com/G5/microformats2).
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,8 +1,11 @@
 import os, json
+
+import nose.tools as n
+
 import ufs2
 
-def test_everything():
-    CASEDIR = os.path.join("support", "cases")
+def test_stuff():
+    CASEDIR = os.path.join("tests", "support", "cases")
     websites = os.listdir(CASEDIR)
     for website in websites:
         website_dir = os.path.join(CASEDIR, website)
@@ -10,11 +13,14 @@ def test_everything():
             sub_website_dir = os.path.join(website_dir, sub_website)
             basenames = set([filename.split('.')[0] for filename in os.listdir(sub_website_dir)])
             for basename in basenames:
-                js_file = open(os.path.join(sub_website_dir, basename + '.js'))
-                html_file = open(os.path.join(sub_website_dir, basename + '.html'))
+                yield check_case, sub_website_dir, basename
 
-                js_file.readline() # Burn the comment
-                expected = json.load(js_file)
+def check_case(sub_website_dir, basename):
+    js_file = open(os.path.join(sub_website_dir, basename + '.js'))
+    html_file = open(os.path.join(sub_website_dir, basename + '.html'))
 
-                observed = ufs2.parse(html_file)
-                n.assert_dict_equal(observed, expected)
+    js_file.readline() # Burn the comment
+    expected = json.load(js_file)
+
+    observed = ufs2.parse(html_file)
+    n.assert_dict_equal(observed, expected)
